c2fc48311d671315f78b0088c799dc1ba9f68ac5
clicube <clicube@gmail.com>
2020-10-15 00:27:44 +0900

    return html page

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-const http = require("http");
+const express = require("express");
 const { networkInterfaces } = require("os");
 const onvif = require("onvif");
-const port = 3333
+const port = 3333;
 
-function main() {
-  process.on("uncaughtException", function (err) {
-    console.log(err);
-  });
-  const serverIpAddress = getLocalIpAddress();
-  startServer(serverIpAddress);
-}
-
-function getLocalIpAddress() {
+const getLocalIpAddress = () => {
   const nets = networkInterfaces();
   const results = {};
 
@@ -29,14 +21,17 @@ function getLocalIpAddress() {
   if (results["en0"] != null) {
     return results["en0"][0];
   }
+  if (results["wlan0"] != null) {
+    return results["wlan0"][0];
+  }
   const keys = Object.keys(results);
   if (results[keys[0]] != null) {
     return results[keys[0]][0];
   }
   return null;
-}
+};
 
-async function getCameraRtspUri() {
+const getCameraRtspUri = async () => {
   return new Promise((resolve, reject) => {
     console.log("Discovering camera ...");
     onvif.Discovery.probe((err, cams) => {
@@ -70,34 +65,33 @@ async function getCameraRtspUri() {
       });
     });
   });
-}
+};
 
-function startServer(serverIpAddress) {
-  http
-    .createServer(async (req, res) => {
-      console.log("Received request.")
-      try {
-        const rtspUri = await getCameraRtspUri();
-        const uri = `vlc://${rtspUri}`
-        res.writeHead(302, {
-          Location: rtspUri,
-        });
-        res.end();
-        console.log(`Sent 302 response. ${uri}`);
-      } catch (err) {
-        console.log(err);
-        res.writeHead(500);
-        res.write(err.toString());
-        res.write("\n\n");
-        res.write(err.message);
-        res.write("\n");
-        res.write(err.stack);
-        res.end();
-      }
-    })
-    .listen(port, () => {
-      console.log(`Server started. http://${serverIpAddress}:${port}`);
-    });
-}
+const app = express();
+app.use(express.static("public"));
 
-main();
+app.get("/", (req, res) => {
+  req.url = "/index.html";
+  app.handle(req, res);
+});
+
+app.get("/streamUri", (req, res) => {
+  (async () => {
+    try {
+      const uri = await getCameraRtspUri();
+      res.send({ uri, result: "OK" });
+    } catch (err) {
+      res.status(500).send({
+        result: "NG",
+        message: err.message,
+        stack: err.stack,
+      });
+    }
+  })();
+});
+
+const serverIpAddress = getLocalIpAddress();
+
+app.listen(port, () => {
+  console.log(`Server started. http://${serverIpAddress}:${port}`);
+});
